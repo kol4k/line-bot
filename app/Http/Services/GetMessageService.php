33ee35c211b4b1605802a79c4bb1ed521363dea4
rafinyadi <rafinyadi@gmail.com>
2017-12-16 07:06:20 -0500

    ckc

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -29,21 +29,15 @@ class GetMessageService
         $replyToken = $formData['events']['0']['replyToken'];
         $api = 'trnsl.1.1.20171216T092715Z.18943ca79fdb501d.84c04771f13b9fc5fad54f2d9084479cb942eb7a';
         $service_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?lang=id-en&key='.$api.'&text='.$replyToken;
-        $curl = curl_init($service_url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $msgResponse = curl_exec($curl);
-        if ($curl_response === false) {
-            $info = curl_getinfo($curl);
-            curl_close($curl);
-            die('error occured during curl exec. Additioanl info: ' . var_export($info));
-        }
-
-        curl_close($curl); 
+        $json = file_get_contents($service_url);
+        $obj = json_decode($json); 
+        $text = $obj->text; 
+        
         
         $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
         $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
         
-        $response = $this->bot->replyText($replyToken, $msgResponse[0]->text);
+        $response = $this->bot->replyText($replyToken, $text[0]);
         
         if ($response->isSucceeded()) {
             logger("reply success!!");
