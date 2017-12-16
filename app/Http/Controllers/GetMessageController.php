f61294710d4c727cd943c0e3faf84062a5c29da1
rafinyadi <rafinyadi@gmail.com>
2017-12-16 06:10:37 -0500

    fix curl

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -35,6 +35,7 @@ class GetMessageController
 
     public function test()
     {
+      $api = 'trnsl.1.1.20171216T092715Z.18943ca79fdb501d.84c04771f13b9fc5fad54f2d9084479cb942eb7a';
         $service_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?lang=id-en&key='.$api.'&text=apa';
         $curl = curl_init($service_url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
