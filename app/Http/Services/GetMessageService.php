78bcb7249a2170bf809bcc73ae00ecd8d81de44e
rafinyadi <rafinyadi@gmail.com>
2017-12-16 07:16:35 -0500

    xxx

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -26,12 +26,12 @@ class GetMessageService
     
     public function replySend($formData)
     {
-        $replyToken = $formData['events']['0']['replyToken'];
+        $replyToken = $formData['events']['0'];
         $api = 'trnsl.1.1.20171216T092715Z.18943ca79fdb501d.84c04771f13b9fc5fad54f2d9084479cb942eb7a';
         $service_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?lang=id-en&key='.$api.'&text='.$replyToken;
-        $json = file_get_contents($service_url);
-        $obj = json_decode($json); 
-        $text = $obj->text; 
+        $jsonx = file_get_contents($service_url);
+        $objx = json_decode($json); 
+        $text = $objx->text; 
         
         
         $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
