2f1c27d1bcf50d7a00a0d97a879852686f5a2236
kol4k <rafinyadi@gmail.com>
2017-12-16 15:44:06 +0700

    sensor beta

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -26,7 +26,7 @@ class GetMessageService
         $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
         
         if (in_array($replyToken, $class_helper->whereWords()->harsh)) {
-            $response = $this->bot->replyText($replyToken, $);
+            $response = $this->bot->replyText($replyToken, "Sesungguhnya tidak ada sesuatu apapun yang paling berat ditimbangan kebaikan seorang mu'min pada hari kiamat seperti akhlaq yang mulia, dan sungguh-sungguh (benar-benar) Allāh benci dengan orang yang lisānnya kotor dan kasar.(Hadīts Riwayat At Tirmidzi nomor 2002, hadīts ini hasan shahīh, lafazh ini milik At Tirmidzi, lihat Silsilatul Ahādīts Ash Shahīhah no 876)");
         }
         if ($response->isSucceeded()) {
             logger("reply success!!");
